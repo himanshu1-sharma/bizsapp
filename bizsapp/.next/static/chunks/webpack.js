a70d983f20e51747f5efb7501f416e645b0ddfdc
Himanshu <sharmagi1998@gmail.com>
2022-07-06 10:00:27 +0530

    card change

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70d48288d0410a2e"; }
+/******/ 		__webpack_require__.h = function() { return "6587811773fd3e18"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
