66eee9fc00201621426429e18bab6870889b3acd
Himanshu <sharmagi1998@gmail.com>
2022-07-07 18:01:53 +0530

    cs

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "456b783546c95a18"; }
+/******/ 		__webpack_require__.h = function() { return "0481ab8541ff5de2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ bizsapp/pages/contact.js
@@ -0,0 +1,88 @@
+import Navigation from "./includes/Navigation";
+import BeforeFooter from "./includes/BeforeFooter";
+import Footer from "./includes/Footer";
+import styles from "../styles/Contact.module.css";
+import { useState } from "react";
+
+export default function contact() {
+  const [user, setUser] = useState({ name: "", email: "", number: "" });
+ 
+let name, value; 
+  const handleInputs = (e) => {
+    console.log(e)
+    name = e.target.name;
+    value = e.target.value;
+    setUser([{...user, [name]: value}])
+  }
+
+  return (
+    <>
+      <Navigation />
+      <div className={styles.contact_form}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <form>
+                <div className={styles.form_group}>
+                  <div className="mb-3">
+                    <label for="name" className="form-label">
+                      Your Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      id="name"
+                      placeholder="Your Name"
+                      value={user.name}
+                      onChange={handleInputs}
+                    />
+                  </div>
+                </div>
+                <div className={styles.form_group}>
+                  <div className="mb-3">
+                    <label for="email" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Email"
+                      value={user.email}
+                      onChange={handleInputs}
+                    />
+                  </div>
+                </div>
+                <div className={styles.form_group}>
+                  <div className="mb-3">
+                    <label for="number" className="form-label">
+                      Phone Number
+                    </label>
+                    <input
+                      type="number"
+                      name="number"
+                      className="form-control"
+                      id="number"
+                      placeholder="Phone Number"
+                      value={user.number}
+                      onChange={handleInputs}
+                    />
+                  </div>
+                </div>
+                <div className={styles.form_group}>
+                  <div className="mb-3">
+                    <button className="btn btn-dark">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <BeforeFooter />
+      <Footer />
+    </>
+  );
+}
