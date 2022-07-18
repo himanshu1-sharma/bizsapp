06ad3e07dc7ec5432f2aacdc6febf5a4078ddf2c
Himanshu <sharmagi1998@gmail.com>
2022-07-18 16:43:51 +0530

    ch

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5bc3d6280c0740a2"; }
+/******/ 		__webpack_require__.h = function() { return "7d61428eb96db4a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- bizsapp/.next/static/webpack/webpack.1aa388fa79b814a1.hot-update.js
+++ bizsapp/.next/static/webpack/webpack.1aa388fa79b814a1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c65fd09c740d7feb"; }
+/******/ 	__webpack_require__.h = function() { return "72836cb45439cf94"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
