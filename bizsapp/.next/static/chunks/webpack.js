36106c8ca1416a19354d8bd1f4b186c98cf98c15
Himanshu <sharmagi1998@gmail.com>
2022-07-22 17:17:12 +0530

    bmc

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f11b0afcdffbb210"; }
+/******/ 		__webpack_require__.h = function() { return "a738db1d688a9dbc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- bizsapp/pages/BMC/UserCard.js
+++ bizsapp/pages/BMC/UserCard.js
@@ -96,13 +96,13 @@ export default function UserCard({ properties, userData }) {
 
                                             <div className={styles.detail_box}>
                                                 <div className={`${styles.social_link} ${styles.social_link_box}`}>
-                                                    {console.log('properties.socialLinks.link', properties.socialLinks.link)}
+                                                    {/* {console.log('properties.socialLinks.link', properties.socialLinks.link)} */}
 
                                                     <ul>
 
                                                         {properties.socialLinks[0].socialLinkType === "Facebook" && properties.socialLinks[0].link !== " " ?
 
-                                                            <a href={properties.socialLinks.link}>
+                                                            <a href={properties.socialLinks[0].link}>
                                                                 <li>
                                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" /></svg>
                                                                 </li>
