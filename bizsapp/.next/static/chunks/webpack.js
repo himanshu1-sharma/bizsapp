a010576d8538535e149ab539a396920b264aefe3
Himanshu <sharmagi1998@gmail.com>
2022-09-17 10:00:20 +0530

    new card

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3544e0efc20ebe90"; }
+/******/ 		__webpack_require__.h = function() { return "a14617b67cdd2c60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- bizsapp/.next/static/webpack/webpack.7e5484e31ff95d2d.hot-update.js
+++ bizsapp/.next/static/webpack/webpack.7e5484e31ff95d2d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f4e0728283507411"; }
+/******/ 	__webpack_require__.h = function() { return "d5d97e6624f60a14"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
