6c80d7aef75c2cb5d3918f1f66b4f46d01a30134
Himanshu <sharmagi1998@gmail.com>
2023-01-23 10:48:50 +0530

    change in bmc new

--- bizsapp/.next/static/chunks/webpack.js
+++ bizsapp/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "333edaaafba65779"; }
+/******/ 		__webpack_require__.h = function() { return "9a5fc3c51aa29f53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
